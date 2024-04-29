ada69e62941c903d19a041448245d22d7982c3ba
Tshaan1104 <159934743+Tshaan1104@users.noreply.github.com>
2024-04-29 13:32:58 +0000

    database changes

--- database.py
+++ database.py
@@ -1,19 +1,9 @@
 
 from sqlalchemy import create_engine,text
 import pymysql
-# print(pymysql.__version__)
+
 engine = create_engine('mysql+pymysql://root:XHTZVHoVvwBlLqjIEWSOCyzwpXaWwUHu@monorail.proxy.rlwy.net:51527/railway')
 
-# with engine.connect() as conn:
-#   result = conn.execute(text("select * from website_first.job"))
-#   result_all=result.all()
-#   # print(type(result.all[0])
-#   # first_result_dict=result_all[0]._asdict()  #_asdict for the conversion of dictionary
-#   result_dicts=[]
-#   for row in result_all:
-#     result_dicts.append(row._asdict())  #_asdict for the conversion to the dictionary
-
-# print(result_dicts,end="/n")
 
 
 def load_jobs_from_db():
